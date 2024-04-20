244b7df5954375c9b3cd459237e11d17281be081
DavidTchintcharauli <chincharaulidavid@gmail.com>
2024-04-20 17:26:17 +0400

    add Portfolio.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import Portfolio from './pages/Portfolio';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
 
 const App = () => {
   return (
